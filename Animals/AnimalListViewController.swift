00a06856f17ab380198e1addca777ab131210e08
jicaey <michaelyoung.dev@gmail.com>
2016-10-06 18:18:32 -0400

    Lab Complete

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,31 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    var animalType = ["Dog", "Cat", "Mouse", "Hamster", "Bunny", "Panda", "Lion", "Pig", "Frog", "Octopus"]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalType.count
+    }
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        let animalName = animalType[indexPath.row]
+        cell.textLabel?.text = animalName
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "AnimalDetailView" { return }
+        let dest = segue.destination as! AnimalViewController
+            let indexPath = tableView.indexPathForSelectedRow
+        if let indexPathUnwrapped = indexPath {
+            dest.animals = animalType[indexPathUnwrapped.row]
+        }
+    }
+
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,51 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    @IBOutlet weak var imageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var animals: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let animals = animals {
+            nameLabel?.text = animals
+            
+            let animalName = englishAnimalName(animals)
+            imageLabel?.text = animalName
+            navigationItem.title = animals
+
+        }
+    }
+    
+        
+        func englishAnimalName(_ animal: String) -> String {
+            switch animal {
+            case "Dog":
+                return "🐶"
+            case "Cat":
+                return "🐱"
+            case "Mouse":
+                return "🐭"
+            case "Hamster":
+                return "🐹"
+            case "Bunny":
+                return "🐰"
+            case "Panda":
+                return "🐼"
+            case "Lion":
+                return "🦁"
+            case "Pig":
+                return "🐷"
+            case "Frog":
+                return "🐸"
+            case "Octopus":
+                return "🐙"
+            default:
+                return ""
+            }
+        }
+
+        
 }
